c41a05af5c13549759d7f14eeb4cadc127091e37
sogaani <soga.ani@gmail.com>
2018-03-19 20:07:17 +0900

    import config from enviroment to support heroku

--- cloud/config-provider.js
+++ cloud/config-provider.js
@@ -1,13 +1,23 @@
 'use strict';
 
-const option = require('../config.json');
+let option;
+try {
+  option = require('../config.json');
+} catch (e) {
+  if (e.code !== 'MODULE_NOT_FOUND') {
+    throw e;
+  }
+
+  option = process.env;
+}
 
 var Config = {
-  devPort        : '31338',
-  clientId       : 'hello',
-  clientSecret   : 'dafD8jraghakjnewuac',
-  homeGraphApiKey: '<API_KEY>',
-  isLocal        : false,
+  devPort             : '31338',
+  clientId            : 'hello',
+  clientSecret        : 'dafD8jraghakjnewuac',
+  homeGraphApiKey     : '<API_KEY>',
+  isLocal             : false,
+  googleServiceAccount: {},
 };
 
 Config = Object.assign(Config, option);
